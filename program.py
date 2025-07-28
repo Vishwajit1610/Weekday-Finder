89abd3b91352f7ab81ea153e6b837cb6ff076c04
Vishwajit <moholvishwajeet@gmail.com>
2025-07-28 16:38:57 +0530

    added input function

---
+++ program.py
@@ -0,0 +1,12 @@
+
+
+# input method defined
+def get_day_of_week():
+    
+    years = int(input("Enter Years:"))
+    completed_years =  years - 1
+
+    print("You entered:", years)
+    print("Therefore completed years:", completed_years)
+
+get_day_of_week()
